0a2ced0e78e2325d0a8130105395fa752efff43f
julietoge <julietogechi27@gmail.com>
2024-07-08 16:03:10 +0100

    update

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import Button from "../buttonComp";
 import "./HeroSection.css";
 
-// HeroSection Component
 const HeroSection = () => {
   return (
-    // rendering of hero section
     <div className="hero-section">
-      {/* hero section text */}
       <p>T-shirt / Tops /Jacket</p>
       <p>Where Fashion Meets Function</p>
       <p>cool / Style / Dress</p>
-      {/* button at hero section */}
       <Button to={"/products"} linkText={"Shop Now"} />
     </div>
   );
