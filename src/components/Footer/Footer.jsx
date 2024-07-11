f17b2ebcd755bdbd619a34fa6aa74ea74c2d401c
julietoge <julietogechi27@gmail.com>
2024-07-11 01:39:46 +0100

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,19 +15,38 @@ const Footer = () => {
             </Link>
           </div>
           <p>
-            <strong>Address:</strong>
+            <strong>Address: </strong>
             <span>Lahore Road, Sheikhupura</span>
           </p>
 
           <p>
-            <strong>E-mail:</strong>
+            <strong>E-mail: </strong>
             <span>Example@gmail.com</span>
           </p>
 
           <p>
-            <strong>Phone:</strong>
+            <strong>Phone: </strong>
             <span>0092 356 3656210</span>
           </p>
+        </div>
+        <div className="Shopping-Categories">
+          <h4>Shopping and Categories</h4>
+          <Link to="/category/Men's%20Collections">Men's Collections</Link>
+          <Link to="/category/Women's%20Collections">Women's Collections</Link>
+          <Link to="/category/Kids%20Collections">Kids' Collections</Link>
+        </div>
+        <div className="Useful-Links">
+          <h4>Useful Links</h4>
+          <Link to="/">Homepage</Link>
+          <Link to="/">About Us</Link>
+          <Link to="/">Help</Link>
+          <Link to="/">Contact Us</Link>
+        </div>
+        <div className="Help-Information">
+          <h4>Help & Information</h4>
+          <Link to="/">FAQ’S</Link>
+          <Link to="/">Shipping</Link>
+          <Link to="/">Tracking Id</Link>
         </div>
       </div>
 
